3739a5f7e2bf214064cf637a9f7590d34688dc80
tylergreason <tyler.greason@protonmail.com>
2020-02-10 16:45:40 -0500

    Finished basic validations

--- art_forum/app/controllers/images_controller.rb
+++ art_forum/app/controllers/images_controller.rb
@@ -1,13 +1,14 @@
 class ImagesController < ApplicationController
     def index
-        @images = Image.all
+        # @images = Image.all
+        @images = Image.sort_by_creation_date
     end
 
     def show
         @image = Image.find(params[:id])
         @comment = Comment.new
     end
-    
+
     private 
     def image_params
         params.require(:image).permit(:url, :metro_link, :artist, :date_created, :title, :user_ids, :comments)
--- art_forum/app/models/image.rb
+++ art_forum/app/models/image.rb
@@ -4,9 +4,19 @@ class Image < ApplicationRecord
 
     validates :url, :metro_link, :artist, :date_created, :title, presence: true 
 
+    # print date in readable format
+    def image_date 
+        self.created_at.strftime("%B %d, %Y")
+    end
+
     #define method for creating random dates. From: 
     # https://stackoverflow.com/questions/4894198/how-to-generate-a-random-date-in-ruby
     def self.random_time from = 0.0, to = Time.now
             Time.at(from + rand * (to.to_f - from.to_f))
+    end
+
+    # sort all images by the date they were made 
+    def self.sort_by_creation_date 
+        self.all.sort_by{|image| image.created_at}
     end
 end
